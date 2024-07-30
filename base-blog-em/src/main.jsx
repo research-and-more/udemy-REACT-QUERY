61fb8cf2830f4b51be489c2257d5bd1ea753f710
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-29 19:37:44 -0600

    feat: class 1.7

--- base-blog-em/src/main.jsx
+++ base-blog-em/src/main.jsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
